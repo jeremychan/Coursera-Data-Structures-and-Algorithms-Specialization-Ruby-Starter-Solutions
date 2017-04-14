1bda403c85dd628b0a54f7ceee59f91e67fb680e
Jeremy <jeremy@jeremychan.net>
2017-04-14 08:20:15 +0800

    added graph algorithms asg1 connected components

---
+++ algorithms-on-graphs/connected_components/connected_components.rb
@@ -0,0 +1,38 @@
+class Vertex
+  attr_accessor :id, :adj
+
+  def initialize(id)
+    @id = id
+    @adj = []
+  end
+
+  def add_adj(id)
+    @adj.push(id)
+  end
+end
+
+class Maze
+  def initialize(n)
+    @vertexes = Array.new(n) { |i| Vertex.new(i) }
+  end
+
+  def add_edge(u, v)
+    @vertexes[u].add_adj(v)
+    @vertexes[v].add_adj(u)
+  end
+
+  def connected_components
+    # write code here
+    0
+  end
+end
+
+if __FILE__ == $0
+  n, m = STDIN.gets.split.map(&:to_i)
+  maze = Maze.new(n)
+  (0...m).each {
+    u, v = STDIN.gets.split.map { |x| x.to_i-1 }
+    maze.add_edge(u, v)
+  }
+  puts "#{maze.connected_components}"
+end
